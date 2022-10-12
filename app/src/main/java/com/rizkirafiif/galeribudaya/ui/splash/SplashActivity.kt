03b716ac7e6e74a8f36b8769396d816a38f6ae38
rizkirafiif <rizkirafiif@gmail.com>
2022-10-12 08:23:03 +0700

    splash

---
+++ app/src/main/java/com/rizkirafiif/galeribudaya/ui/splash/SplashActivity.kt
@@ -0,0 +1,12 @@
+package com.rizkirafiif.galeribudaya.ui.splash
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.rizkirafiif.galeribudaya.R
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+    }
+}
