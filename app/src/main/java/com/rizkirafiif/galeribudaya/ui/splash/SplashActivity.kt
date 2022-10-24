500383984221751b08f7f630cb9098ef0a050b16
rizkirafiif <rizkirafiif@gmail.com>
2022-10-24 21:43:17 +0700

    splash

--- app/src/main/java/com/rizkirafiif/galeribudaya/ui/splash/SplashActivity.kt
+++ app/src/main/java/com/rizkirafiif/galeribudaya/ui/splash/SplashActivity.kt
@@ -3,10 +3,13 @@ package com.rizkirafiif.galeribudaya.ui.splash
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.rizkirafiif.galeribudaya.R
+import com.rizkirafiif.galeribudaya.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
+    private lateinit var binding: ActivitySplashBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
 }
