0527c25ed336da72eccbc4aceb297607e4b7e3d5
Rizki Rafiif <58683476+rizki-rafiif@users.noreply.github.com>
2022-10-24 17:40:32 +0700

    binding

--- app/src/main/java/com/rizkirafiif/galeribudaya/ui/home/HomeFragment.kt
+++ app/src/main/java/com/rizkirafiif/galeribudaya/ui/home/HomeFragment.kt
@@ -6,21 +6,17 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.rizkirafiif.galeribudaya.R
+import com.rizkirafiif.galeribudaya.databinding.FragmentHomeBinding
 
-class HomeFragment : Fragment() {
+class HomeFragment : Fragment(R.layout.fragment_home) {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-        }
-    }
+    private lateinit var binding:FragmentHomeBinding
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = FragmentHomeBinding.bind(view)
+
+
     }
 
 }
