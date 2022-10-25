7343a8c0568769c2ba29490576100c17e241c27b
Rizki Rafiif <58683476+rizki-rafiif@users.noreply.github.com>
2022-10-25 17:41:22 +0700

    setup db

---
+++ app/src/main/java/com/rizkirafiif/galeribudaya/db/DatabaseHelper.kt
@@ -0,0 +1,22 @@
+package com.rizkirafiif.galeribudaya.db
+
+import android.provider.BaseColumns
+
+class DatabaseHelper {
+    internal class BudayaColumns : BaseColumns {
+        companion object {
+            const val TABLE_BUDAYA = "budaya"
+            const val _ID = "_id"
+            const val NAME = "nama"
+            const val JENIS = "jenis"
+            const val DAERAH = "daerah"
+            const val DESKRIPSI = "deskripsi"
+            const val VIDEO = "link_video"
+            const val GAMBAR_1 = "link_gambar_1"
+            const val GAMBAR_2 = "link_gambar_2"
+            const val GAMBAR_3 = "link_gambar_3"
+            const val GAMBAR_4 = "link_gambar_4"
+            const val GAMBAR_5 = "link_gambar_5"
+        }
+    }
+}
