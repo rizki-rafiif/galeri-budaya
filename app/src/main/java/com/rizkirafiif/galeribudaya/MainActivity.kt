34f8ab3e6e7144d1899707e8b6013ea34c39f1dd
rizkirafiif <rizkirafiif@gmail.com>
2022-11-03 10:13:34 +0700

    setup navigation

--- app/src/main/java/com/rizkirafiif/galeribudaya/MainActivity.kt
+++ app/src/main/java/com/rizkirafiif/galeribudaya/MainActivity.kt
@@ -2,7 +2,11 @@ package com.rizkirafiif.galeribudaya
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.rizkirafiif.galeribudaya.databinding.ActivityMainBinding
+import com.rizkirafiif.galeribudaya.ui.about.AboutFragment
+import com.rizkirafiif.galeribudaya.ui.browse.BrowseFragment
+import com.rizkirafiif.galeribudaya.ui.home.HomeFragment
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMainBinding
@@ -11,6 +15,25 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        supportActionBar?.hide()
+        val fragmentHome = HomeFragment()
+        val browseFragment = BrowseFragment()
+        val aboutFragment = AboutFragment()
+
+        setCurrentFragment(fragmentHome)
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home_fragment -> setCurrentFragment(fragmentHome)
+                R.id.browse_fragment -> setCurrentFragment(browseFragment)
+                R.id.about_fragment -> setCurrentFragment(aboutFragment)
+            }
+            true
+        }
     }
+
+    private fun setCurrentFragment(fragment: Fragment) =
+        supportFragmentManager.beginTransaction().apply {
+            replace(R.id.fragment_container_view, fragment)
+            commit()
+        }
 }
