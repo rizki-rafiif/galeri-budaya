839481aeba169b1534771cface503e102a0c7f99
rizkirafiif <rizkirafiif@gmail.com>
2022-12-03 20:51:52 +0700

    update db settings

--- app/src/main/java/com/rizkirafiif/galeribudaya/MainActivity.kt
+++ app/src/main/java/com/rizkirafiif/galeribudaya/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.rizkirafiif.galeribudaya
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.fragment.app.Fragment
+import com.rizkirafiif.galeribudaya.Data.DataBudaya
 import com.rizkirafiif.galeribudaya.databinding.ActivityMainBinding
 import com.rizkirafiif.galeribudaya.ui.about.AboutFragment
 import com.rizkirafiif.galeribudaya.ui.browse.BrowseFragment
@@ -27,6 +29,8 @@ class MainActivity : AppCompatActivity() {
             }
             true
         }
+
+        DataBudaya(applicationContext).executeDB()
     }
 
     private fun setCurrentFragment(fragment: Fragment) =
@@ -34,4 +38,6 @@ class MainActivity : AppCompatActivity() {
             replace(R.id.fragment_container_view, fragment)
             commit()
         }
+
+
 }
