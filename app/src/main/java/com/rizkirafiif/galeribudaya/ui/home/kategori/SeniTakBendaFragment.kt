dbaed6c0efa2f3fadb97c2868ff9fbc145147fc2
rizkirafiif <rizkirafiif@gmail.com>
2022-12-04 20:21:57 +0700

    update view on themes and minor change on backend

---
+++ app/src/main/java/com/rizkirafiif/galeribudaya/ui/home/kategori/SeniTakBendaFragment.kt
@@ -0,0 +1,29 @@
+package com.rizkirafiif.galeribudaya.ui.home.kategori
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.rizkirafiif.galeribudaya.R
+import com.rizkirafiif.galeribudaya.databinding.FragmentBendaSeniBinding
+import com.rizkirafiif.galeribudaya.databinding.FragmentSeniTakBendaBinding
+
+class SeniTakBendaFragment : Fragment() {
+    private var _binding: FragmentSeniTakBendaBinding?= null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentSeniTakBendaBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
